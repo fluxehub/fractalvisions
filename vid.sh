7ab90458eb538dba4ee96f514c1d6cfb6defd3d0
fluxehub <53355311+fluxehub@users.noreply.github.com>
2019-11-08 20:50:37 +0000

    yes

--- vid.sh
+++ vid.sh
@@ -2,4 +2,4 @@
 rm *.mp4 out/*
 stack run
 ffmpeg -framerate 60 -i out/frame%04d.png output.mp4
-ffmpeg -i output.mp4 -i test.wav -c:v libx264 -c:a libvorbis -shortest test.mp4
+ffmpeg -i output.mp4 -i song.wav -c:v libx264 -c:a libvorbis -shortest test.mp4
