79f27733de19d4d49758e3c92a0768d5e31c8158
Filip Balucha <balucha.filip2@gmail.com>
2019-11-11 21:36:50 +0000

    heck

--- src/Circle.hs
+++ src/Circle.hs
@@ -1,13 +1,13 @@
-data Circle = 
-     Circle { xCentre :: Double
-            , yCentre :: Double
-            , radius :: Double
-     }
-
+data Circle = Circle { 
+    xCentre :: Double, 
+    yCentre :: Double,
+    radius :: Double
+}
+-- heck
 point :: Circle -> Double
 point circle    | p < 0 = p + (2 * xEdge) + 1
                 | otherwise = p + (2 * (xEdge - yEdge)) + 1
     where
         xEdge = 0
-        yEdge = circle radius
-        p = 1 - (circle radius)
+        yEdge = radius circle
+        p = 1 - (radius circle)
