3e048c921bf8375c90b5585f74755a58d7405450
Advaith Sai <advaith.maddipatla@gmail.com>
2019-11-12 14:28:10 +0000

    silly indentation changed

--- src/Section.hs
+++ src/Section.hs
@@ -64,7 +64,7 @@ genSection (Options frame frameCount depth zoom zoomStep cX cY cXstep cYstep sat
         nFrame = fromIntegral frame
         nDepth = fromIntegral depth
 
-        newcX  = if nFrame >= 0 && nFrame <=124 then sin $ cX + nFrame * cXstep else sin $ nFrame * cXstep 
+        newcX  = if nFrame >= 0 && nFrame <= 124 then sin $ cX + nFrame * cXstep else sin $ nFrame * cXstep 
         newcY  = if nFrame >= 0 && nFrame <= 124 then sin $ cY + nFrame * cYstep else sin $ nFrame * cYstep
 
         -- disable kick events if no kick
