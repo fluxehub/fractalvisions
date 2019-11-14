bc16741325ee48e7c165e201296b436caa7f124d
Advaith Sai <advaith.maddipatla@gmail.com>
2019-11-14 15:38:57 +0000

    Dude no ~ Filip and Advaith

--- src/CircleScaling.hs
+++ src/CircleScaling.hs
@@ -3,13 +3,37 @@ module CircleScaling where
   import Codec.Picture
   -- import Codec.Picture.ScaleDCT (scale)
   import LOL
+  import Text.Printf
   -- import Vision.Image.Transform.ScaleDCT
   
+  loadImage :: IO (Maybe (Image PixelRGBA8))
   loadImage = do
     imageLoad <- readPng "src/circle.png"
-    case imageLoad of
-      Left error -> putStrLn error
-      Right (ImageRGBA8 image) -> do
-        writePng "src/circle2.png" (scaleBilinear 100 100 image)
-
+    return $ case imageLoad of
+      Left _                   -> Nothing
+      Right (ImageRGBA8 image) -> Just image
       
+        
+  
+  scaling :: Int -> Int -> Int -> Int -> IO ()
+  scaling frameNum frameCount dim step = do
+    image <- loadImage
+    case image of
+      Nothing    -> putStrLn "error loading image"
+      Just image -> genCircleFrame image frameNum frameCount dim step
+  
+  genCircleFrame :: (Image PixelRGBA8) -> Int -> Int -> Int -> Int -> IO()
+  genCircleFrame image frameNum frameCount dim step = do
+    writePng (printf "images/circle%04d.png" (frameNum+1)) (scaleBilinear dim dim image)
+    if (frameNum+1) == frameCount
+      then return()
+      else genCircleFrame image (frameNum + 1) frameCount (dim + step) step
+            -- putStrLn $ printf "rendering circle %d" (frame+1)
+            -- writePng (printf "images/circle%04d.png" frame) (scaleBilinear dim dim image)
+
+    
+
+  
+  
+    
+  
