545970fe4601f1848644dcfe9fd6ad2a0c12920f
fluxehub <53355311+fluxehub@users.noreply.github.com>
2019-11-14 18:30:30 +0000

    some revision to section

--- src/Section.hs
+++ src/Section.hs
@@ -3,7 +3,8 @@
 module Section (
     introA,
     introB,
-    verseA
+    verse1,
+    bridge1A
 ) where
 
 import Codec.Picture
@@ -17,19 +18,17 @@ import qualified Processing
 
 data Options = Options { 
     frame      :: Int,
-    count      :: Int,
+    out        :: Int,
     depth      :: Int,
     zoom       :: Double,
     zoomStep   :: Double,
-    cX         :: Double,
-    cY         :: Double,
     cXstep     :: Double,
     cYstep     :: Double,
     saturation :: Double,
     kick       :: Bool
 } deriving (Show, Eq)
 
-quarterNotes = map (\x -> 570 + round (x * (3600.0/130.0))) [0..20000]
+quarterNotes = map (\x -> 569 + round (x * (3600.0/130.0))) [0..20000]
 
 genFrame :: Julia -> Int -> Double -> IO ()
 genFrame f frame sat = do
@@ -49,7 +48,7 @@ genFrame f frame sat = do
         frameF     = generateImage f' w h
 
 genSection :: Options -> IO Options
-genSection (Options frame frameCount depth zoom zoomStep cX cY cXstep cYstep sat kick) = do
+genSection (Options frame out depth zoom zoomStep cXstep cYstep sat kick) = do
         -- define fractal
         let f = Julia 200 200 zoom newcX newcY depth
         
@@ -57,17 +56,17 @@ genSection (Options frame frameCount depth zoom zoomStep cX cY cXstep cYstep sat
         genFrame f frame sat
 
         -- re-run with new params if frames left to render
-        if (frame + 1) == frameCount
+        if (frame + 1) == out
         then 
-            return $ Options frame frameCount depth zoom zoomStep cX cY cXstep cYstep sat kick
+            return $ Options frame out depth zoom zoomStep cXstep cYstep sat kick
         else 
-            genSection $ Options (frame + 1) frameCount newDepth newZoom zoomStep newcX newcY cXstep cYstep sat kick
+            genSection $ Options (frame + 1) out newDepth newZoom zoomStep cXstep cYstep sat kick
     where
         nFrame = fromIntegral frame
         nDepth = fromIntegral depth
 
-        newcX  = if nFrame >= 0 && nFrame <= 124 then sin $ cX + nFrame * cXstep else sin $ nFrame * cXstep 
-        newcY  = if nFrame >= 0 && nFrame <= 124 then sin $ cY + nFrame * cYstep else sin $ nFrame * cYstep
+        newcX  = sin $ nFrame * cXstep
+        newcY  = sin $ nFrame * cYstep
 
         -- disable kick events if no kick
         kickFrames = if kick then quarterNotes else [-1]
@@ -79,12 +78,16 @@ genSection (Options frame frameCount depth zoom zoomStep cX cY cXstep cYstep sat
             | otherwise = if frame `elem` kickFrames then zoom + (zoomStep * 10) else zoom + zoomStep
 
 introA :: IO Options
-introA = genSection (Options 0 126 10 0.3 0.0001 0 0.3 0 0 0 False)
+introA = genSection (Options 0 126 10 0.3 0.0001 0 0 0 False)
 
 introB :: Options -> IO Options
-introB (Options frame _ depth zoom zoomStep cX cY _ _ s _) =
-    genSection (Options frame (444 + frame) depth zoom zoomStep cX cY 0.0001 0.001 s False)
+introB (Options frame _ depth zoom zoomStep _ _ s _) =
+    genSection (Options frame 569 depth zoom zoomStep 0.0001 0.001 s False)
 
-verseA :: Options -> IO Options
-verseA (Options frame _ depth zoom zoomStep cX cY cXstep cYstep s _) =
-    genSection (Options frame (3150 + frame) depth zoom zoomStep cX cY cXstep cYstep s True)
+verse1 :: Options -> IO Options
+verse1 (Options frame _ depth zoom zoomStep cXstep cYstep s _) =
+    genSection (Options frame 4114 depth zoom zoomStep cXstep cYstep s True)
+
+bridge1A :: Options -> IO Options
+bridge1A (Options frame _ depth zoom _ _ _ s _) =
+    genSection (Options frame 4557 depth zoom 0.0005 0.00015 0.0015 s True)
