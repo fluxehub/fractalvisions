9a978a019a776b53faffc2aa0bb54ad26096a2d3
Filip Balucha <balucha.filip2@gmail.com>
2019-11-14 21:19:44 +0000

    wtf2

--- src/DrawBitmap.hs
+++ src/DrawBitmap.hs
@@ -8,14 +8,13 @@ module DrawBitmap where
   import Text.Printf
 
   -- legacy code by Filip
-  drawCircle :: Int -> IO()
-  drawCircle radius = do    
-    writePng "../test/TEST.png" $ generateImage pixelRenderer w h
+  drawCircle :: Int -> Int -> Int -> IO()
+  drawCircle frameSize radius thickness = do    
+    writePng "../test/TEST.png" $ generateImage pixelRenderer frameSize frameSize
     where
-      thickness = 10
-      centre = (w `div` 2,h `div` 2)
+      centre = (frameSize`div`2,frameSize`div`2)
       circlePoints = concatMap (generateCirclePoints centre) [((radius-thickness) `div` 2)..((radius + thickness) `div` 2)]
       pixelRenderer x y | (x,y) `elem` circlePoints   = PixelRGB8 0   0   0
                         | otherwise                   = PixelRGB8 255 255 255
-      w = 500
-      h = 500
+
+  
