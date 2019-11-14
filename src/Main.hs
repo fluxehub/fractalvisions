b405ec5385e997123e1d2e1fdcabc42ac690ebba
fluxehub <53355311+fluxehub@users.noreply.github.com>
2019-11-14 20:07:40 +0000

    sections aoehrwfewioahf

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,15 @@ main = do
   iB <- introB iA
   v1 <- verse1 iB
   b1a <- bridge1A v1
-  b1b <- bridge1B b1a
-  b1c <- bridge1C b1b
+  b1b <- bridge2A b1a
+  b1c <- bridge2A b1b
+  c1 <- chorus1 b1c
+  ce <- chorusend c1
+  b2 <- bridge2 ce
+  v2a <- verse2a b2
+  v2b <- verse2b v2a
+  v2c <- verse2c v2b
+  pc <- prechorus v2c
+  c2 <- chorus2 pc
+  outro <- c2
   return ()
